f25fcce20f6a32046df643a2fb478f209dc81324
Johan Östling <johan.f.ostling@gmail.com>
2022-05-07 15:15:07 +0200

    Switch UI to UI Toolkit
    Implement FPS counter in UI Toolkit

---
+++ Assets/Scripts/UI/FPSController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class FPSController
+    {
+        private readonly Label fpsLabel;
+        
+        private float elapsed;
+        private int frames;
+        
+        public FPSController(VisualElement root)
+        {
+            fpsLabel = root.Q<Label>("FPS");
+            ShowFPS(0);
+        }
+
+        public void AddFrameTime(float frameTime)
+        {
+            frames++;
+            elapsed += frameTime;
+
+            if (!(elapsed > 1f)) return;
+            
+            var fps = Mathf.RoundToInt(frames / elapsed);
+            frames = 0;
+            elapsed = 0;
+            ShowFPS(fps);
+        }
+
+        private void ShowFPS(int fps)
+        {
+            fpsLabel.text = $"FPS: {fps}";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/FPSView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class FPSView : MonoBehaviour
+    {
+        public UIDocument ui;
+        private FPSController controller;
+        private void OnEnable()
+        {
+            controller = new(ui.rootVisualElement);
+        }
+
+        private void Update()
+        {
+            controller.AddFrameTime(Time.deltaTime);
+        }
+    }
+}
