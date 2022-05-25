b08bd38344db6b658b2b00de2cd8c172da4af00e
Johan Östling <johan.f.ostling@gmail.com>
2022-05-25 22:23:58 +0200

    Add UI display for player health

---
+++ Assets/Scripts/UI/PlayerHealthController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class PlayerHealthController
+    {
+        private readonly VisualElement heartContainer;
+        private readonly VisualTreeAsset heartTemplate;
+
+        public PlayerHealthController(VisualElement root, VisualTreeAsset template)
+        {
+            heartContainer = root.Q<VisualElement>("HealthBar");
+            heartTemplate = template;
+        }
+
+        public void ShowHealth(int value)
+        {
+            heartContainer.Clear();
+            if (value <= 0) return;
+            
+            var hearts = value / 8;
+            for (var i = 0; i < hearts; i++)
+            {
+                var fullHeart = heartTemplate.Instantiate();
+                fullHeart.AddToClassList("heart-0");
+                heartContainer.Add(fullHeart);
+            }
+
+            var remaining = value % 8;
+            if (remaining <= 0) return;
+
+            var partialHeart = heartTemplate.Instantiate();
+            partialHeart.AddToClassList($"heart-{remaining}");
+            heartContainer.Add(partialHeart);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PlayerHealthView.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using Variables;
+
+namespace UI
+{
+    public class PlayerHealthView : MonoBehaviour
+    {
+        public IntVariable currentHealth;
+        public VisualTreeAsset heartTemplate;
+        
+        private PlayerHealthController playerHealthController;
+        private int lastDisplayedHealth;
+        
+        private void OnEnable()
+        {
+            var ui = GetComponent<UIDocument>();
+            playerHealthController = new (ui.rootVisualElement, heartTemplate);
+            lastDisplayedHealth = int.MinValue;
+        }
+
+        private void Update()
+        {
+            if (currentHealth.Value == lastDisplayedHealth) return;
+            
+            playerHealthController.ShowHealth(currentHealth.Value);
+            lastDisplayedHealth = currentHealth.Value;
+        }
+    }
+}
