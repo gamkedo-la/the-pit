3836de1719f43393cb2da5d7b686d787f4d1281a
Johan Östling <johan.f.ostling@gmail.com>
2022-05-26 08:21:53 +0200

    Move MusicToggle script to Audio namespace

---
+++ Assets/Scripts/Audio/MusicToggle.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Audio
+{
+    public class MusicToggle : MonoBehaviour
+    {
+        [SerializeField] private AudioSource musicSource;
+
+        private bool PlayingMusic
+        {
+            get => !musicSource.mute;
+            set => musicSource.mute = !value;
+        }
+
+        void Update()
+        {
+            if (PlayingMusic && Input.GetKeyDown(KeyCode.M))
+            {
+                MuteMusicAudio();
+            }
+
+            else if (!PlayingMusic && Input.GetKeyDown(KeyCode.M))
+            {
+                UnMuteMusicAudio();
+            }
+        }
+
+        private void MuteMusicAudio()
+        {
+            PlayingMusic = false;
+        }
+
+        private void UnMuteMusicAudio()
+        {
+            PlayingMusic = true;
+        }
+    }
+}
