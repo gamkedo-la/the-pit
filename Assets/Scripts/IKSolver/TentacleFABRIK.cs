92b1e2339f1be0d5581cf03a4e8c8a85e994b1c4
Evan Sklarski <esklarski@gmail.com>
2022-05-27 11:42:47 -0700

    Minimum IKSolver implementation. Do not use, it does nothing but throw exceptions atm.

---
+++ Assets/Scripts/IKSolver/TentacleFABRIK.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.IK;
+
+public class TentacleFABRIK : Solver2D
+{
+    public override IKChain2D GetChain(int index)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override int GetChainCount()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void DoUpdateIK(List<Vector3> effectorPositions)
+    {
+        throw new System.NotImplementedException();
+    }
+}
