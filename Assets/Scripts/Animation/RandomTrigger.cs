2493795f2e95ab32f5a642fc5955c28ec14308d3
Johan Östling <johan.f.ostling@gmail.com>
2022-05-29 14:57:29 +0200

    The Tentacles Have Eyes!

---
+++ Assets/Scripts/Animation/RandomTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Animation
+{
+    public class RandomTrigger : MonoBehaviour
+    {
+        public Animator animator;
+        public TriggerProbability[] triggers;
+
+        private void FixedUpdate()
+        {
+            foreach (var tp in triggers)
+            {
+                if (Mathf.FloorToInt(Random.value * 60f * 100f) <= tp.probabilityPercent)
+                {
+                    animator.SetTrigger(tp.trigger);
+                    break;
+                }
+            }
+        }
+
+        [Serializable]
+        public struct TriggerProbability
+        {
+            public string trigger;
+            [Range(0, 100)]
+            public int probabilityPercent;
+        }
+        
+    }
+}
