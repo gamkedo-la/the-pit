86f045666d76d99642f2601cbbea08bb7d49082d
Evan Sklarski <esklarski@gmail.com>
2022-05-31 00:17:51 -0700

    Copy Unity CCD and FABRIK IK solvers, into scripts we can more easily modify. TentacleCCD and TentacleFABRIK can now be used with no functional change.

--- Assets/Scripts/IKSolver/TentacleCCD.cs
+++ Assets/Scripts/IKSolver/TentacleCCD.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 using UnityEngine.U2D.IK;
 using UnityEngine.Profiling;
 
-[Solver2DMenuAttribute("TentacleFABRIK")]
-public class TentacleFABRIK : Solver2D
+[Solver2DMenuAttribute("TentacleCCD")]
+public class TentacleCCD : Solver2D
 {
     private const float kMinTolerance = 0.001f;
     private const int kMinIterations = 1;
+    private const float kMinVelocity = 0.01f;
+    private const float kMaxVelocity = 1f;
 
     [SerializeField]
     private IKChain2D m_Chain = new IKChain2D();
+
     [SerializeField][Range(kMinIterations, 50)]
     private int m_Iterations = 10;
     [SerializeField][Range(kMinTolerance, 0.1f)]
     private float m_Tolerance = 0.01f;
+    [SerializeField][Range(0f, 1f)]
+    private float m_Velocity = 0.5f;
 
-    private float[] m_Lengths;
-    private Vector2[] m_Positions;
-    private Vector3[] m_WorldPositions;
+    private Vector3[] m_Positions;
 
     /// <summary>
     /// Get and Set the solver's itegration count.
@@ -40,9 +43,18 @@ public class TentacleFABRIK : Solver2D
     }
 
     /// <summary>
+    /// Get and Set the solver velocity.
+    /// </summary>
+    public float velocity
+    {
+        get { return m_Velocity; }
+        set { m_Velocity = Mathf.Clamp01(value); }
+    }
+
+    /// <summary>
     /// Returns the number of chain in the solver.
     /// </summary>
-    /// <returns>This always returns 1.</returns>
+    /// <returns>This always returns 1</returns>
     protected override int GetChainCount()
     {
         return 1;
@@ -57,27 +69,17 @@ public class TentacleFABRIK : Solver2D
     {
         return m_Chain;
     }
-
+    
     /// <summary>
     /// DoPrepare override from base class.
     /// </summary>
     protected override void DoPrepare()
     {
         if (m_Positions == null || m_Positions.Length != m_Chain.transformCount)
-        {
-            m_Positions = new Vector2[m_Chain.transformCount];
-            m_Lengths = new float[m_Chain.transformCount - 1];
-            m_WorldPositions = new Vector3[m_Chain.transformCount];
-        }
+            m_Positions = new Vector3[m_Chain.transformCount];
 
         for (int i = 0; i < m_Chain.transformCount; ++i)
-        {
-            m_Positions[i] = GetPointOnSolverPlane(m_Chain.transforms[i].position);
-        }
-        for (int i = 0; i < m_Chain.transformCount - 1; ++i)
-        {
-            m_Lengths[i] = (m_Positions[i + 1] - m_Positions[i]).magnitude;
-        }
+            m_Positions[i] = m_Chain.transforms[i].position;
     }
 
     /// <summary>
@@ -86,22 +88,18 @@ public class TentacleFABRIK : Solver2D
     /// <param name="effectorPositions">Target position for the chain.</param>
     protected override void DoUpdateIK(List<Vector3> effectorPositions)
     {
-        Profiler.BeginSample("FABRIKSolver2D.DoUpdateIK");
+        Profiler.BeginSample("CCDSolver2D.DoUpdateIK");
 
         Vector3 effectorPosition = effectorPositions[0];
-        effectorPosition = GetPointOnSolverPlane(effectorPosition);
-        if (FABRIK2D.Solve(effectorPosition, iterations, tolerance, m_Lengths, ref m_Positions))
-        {
-            // Convert all plane positions to world positions
-            for (int i = 0; i < m_Positions.Length; ++i)
-            {
-                m_WorldPositions[i] = GetWorldPositionFromSolverPlanePoint(m_Positions[i]);
-            }
+        Vector2 effectorLocalPosition2D = m_Chain.transforms[0].InverseTransformPoint(effectorPosition);
+        effectorPosition = m_Chain.transforms[0].TransformPoint(effectorLocalPosition2D);
 
+        if (CCD2D.Solve(effectorPosition, GetPlaneRootTransform().forward, iterations, tolerance, Mathf.Lerp(kMinVelocity, kMaxVelocity, m_Velocity), ref m_Positions))
+        {
             for (int i = 0; i < m_Chain.transformCount - 1; ++i)
             {
                 Vector3 startLocalPosition = m_Chain.transforms[i + 1].localPosition;
-                Vector3 endLocalPosition = m_Chain.transforms[i].InverseTransformPoint(m_WorldPositions[i + 1]);
+                Vector3 endLocalPosition = m_Chain.transforms[i].InverseTransformPoint(m_Positions[i + 1]);
                 m_Chain.transforms[i].localRotation *= Quaternion.FromToRotation(startLocalPosition, endLocalPosition);
             }
         }
