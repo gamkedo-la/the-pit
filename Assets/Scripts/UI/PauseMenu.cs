5446127d17481cb3980165806b594da8ba2e1fdb
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-06-04 21:29:23 +0500

    Added Pause Menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject btn1;
+    [SerializeField] GameObject btn2;
+
+
+    private void Start()
+    {
+        btn1.SetActive(false);
+        btn2.SetActive(false);
+
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            btn1.SetActive(true);
+            btn2.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void OnResume()
+    {
+        btn1.SetActive(false);
+        btn2.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void OnQuite()
+    {
+        Application.Quit();
+    }
+}
