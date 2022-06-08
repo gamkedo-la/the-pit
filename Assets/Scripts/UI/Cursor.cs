e9ab49346379987b3078b04297a7f94d349fd5ec
Johan Östling <johan.f.ostling@gmail.com>
2022-06-08 19:41:06 +0200

    Add aiming crosshairs as mouse cursor

---
+++ Assets/Scripts/UI/Cursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    [CreateAssetMenu]
+    public class Cursor : ScriptableObject
+    {
+        public Texture2D texture;
+        public Vector2 offset;
+        public CursorMode cursorMode = CursorMode.Auto;
+
+        public void Set()
+        {
+            UnityEngine.Cursor.SetCursor(texture, offset, cursorMode);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIToggler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using Variables;
+
+public class UIToggler : MonoBehaviour
+{
+    public UI.Cursor defaultCursor;
+    public KeyAndTarget[] toggles;
+
+    private void Awake()
+    {
+        defaultCursor.Set();
+        foreach (var keyAndTarget in toggles)
+        {
+            keyAndTarget.SetToDefaultState();
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var keyAndTarget in toggles)
+        {
+            if (keyAndTarget.IsKeyDown)
+            {
+                defaultCursor.Set();
+                keyAndTarget.SetActive(!keyAndTarget.IsActive);
+            }
+        }
+    }
+
+    [Serializable]
+    public struct KeyAndTarget
+    {
+        [SerializeField]
+        private KeyCode key;
+        [SerializeField]
+        private GameObject target;
+        [SerializeField]
+        private BoolVariable defaultState;
+        [SerializeField]
+        private UI.Cursor cursor;
+
+        public bool IsActive => target.activeSelf;
+        public bool IsKeyDown => Input.GetKeyDown(key);
+
+        public void SetActive(bool active)
+        {
+            target.SetActive(active);
+            if (active && cursor != null) cursor.Set();
+        }
+
+        public void SetToDefaultState()
+        {
+            if (defaultState == null) return;
+            SetActive(defaultState.Value);
+        }
+
+    }
+}
