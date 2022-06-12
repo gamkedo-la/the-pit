4baf0260cd1285438d59a1dd1954b475bb185873
Johan Östling <johan.f.ostling@gmail.com>
2022-06-12 12:33:06 +0200

    Add support for multiple weapons and an assault rifle weapon with flashlight mod

---
+++ Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,92 @@
+﻿using Audio;
+using UnityEngine;
+
+namespace Combat
+{
+    public class Weapon : MonoBehaviour
+    {
+        [Header("Weapon stats")] 
+        [Range(0, 100)]
+        public float maxRange = 10;
+        [Min(1)]
+        public int damage;
+
+        public float cooldownBetweenShots = 0.2f;
+        public bool automaticFire;
+        
+        [Header("Sound")] 
+        public AudioSource audioPlayer;
+        public AudioClipWithVolume shootSound;
+        
+        public Transform muzzle;
+        public Animator muzzleFlash;
+        public GameObject projectileImpactPrefab;
+        [Tooltip("How long the projectile impact should last as a game object before being removed")]
+        public float projectileImpactDuration;
+
+        private float activeCooldown;
+        private static readonly int Shoot = Animator.StringToHash("Shoot");
+
+        private void Start()
+        {
+            audioPlayer = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            if (activeCooldown > 0)
+            {
+                activeCooldown -= Time.deltaTime;
+            }
+        }
+
+        public void FireIfPossible(Vector2 aimPoint, float angle)
+        {
+            if (activeCooldown > 0) return;
+            
+            Vector2 muzzlePos = muzzle.transform.position;
+
+            var shotVector = aimPoint - muzzlePos;
+            var shotDirection = shotVector.normalized;
+        
+            // Adjust shot point to max range
+            var dist = shotVector.magnitude;
+            if (dist > maxRange)
+            {
+                shotVector = shotDirection * maxRange;
+            }
+
+            activeCooldown = cooldownBetweenShots;
+            muzzleFlash.SetTrigger(Shoot);
+            
+            audioPlayer.PlayOneShot(shootSound);
+
+            Vector3 impactPos = muzzlePos + shotVector;
+            
+            var hit = Physics2D.Raycast(muzzlePos, shotDirection, maxRange);
+            if (hit.collider != null)
+            {
+                impactPos = hit.point;
+                if (CheckWhatWasHit(hit.collider.gameObject, hit.point))
+                {
+                    return;
+                }
+            }
+
+            impactPos.z = transform.position.z;
+            
+            // Wasteful, use object pool
+            var projectileImpact = Instantiate(projectileImpactPrefab);
+            projectileImpact.transform.position = impactPos;
+            Destroy(projectileImpact, projectileImpactDuration);
+        }
+        
+        private bool CheckWhatWasHit(GameObject hitObject, Vector2 hitPoint)
+        {
+            var health = hitObject.GetComponentInParent<Health>();
+            if (health == null) return false;
+            health.Damage(damage, hitObject, hitPoint);
+            return true;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerWeaponController.cs
+++ Assets/Scripts/Player/PlayerWeaponController.cs
@@ -1,5 +1,4 @@
-﻿using Audio;
-using Combat;
+﻿using Combat;
 using UnityEngine;
 
 namespace Player
@@ -7,15 +6,6 @@ namespace Player
     public class PlayerWeaponController : MonoBehaviour
     {
 
-        [Header("Weapon stats")] 
-        [Range(0, 100)]
-        public float maxRange = 10;
-        [Min(1)]
-        public int damage;
-
-        public float cooldownBetweenShots = 0.2f;
-        public bool automaticFire;
-    
         [Header("Aim arc in degrees")]
         [Range(-90, 90)]
         public int minAim = -90;
@@ -24,34 +14,21 @@ namespace Player
 
         [Range(0, 1)] public float flipDeadZone = 0.1f;
 
-        [Header("Sound")] 
-        public AudioSource audioPlayer;
-        public AudioClipWithVolume shootSound;
-
         [Header("Links")] 
         public Animator bodyAnimator;
         public Transform aimRotationPoint;
-        public Transform muzzle;
-        public Animator muzzleFlash;
-        public GameObject projectileImpactPrefab;
-        [Tooltip("How long the projectile impact should last as a game object before being removed")]
-        public float projectileImpactDuration;
 
-        private float activeCooldown = 0;
-        private static readonly int Shoot = Animator.StringToHash("Shoot");
         private Camera mainCamera;
+        private Weapon weapon;
 
         private void Start()
         {
             mainCamera = Camera.main;
+            weapon = GetComponentInChildren<Weapon>();
         }
 
         private void Update()
         {
-            if (activeCooldown > 0)
-            {
-                activeCooldown -= Time.deltaTime;
-            }
             var aimPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
             var direction = transform.localScale.x;
             if (direction > 0 && aimPosition.x < transform.position.x - flipDeadZone)
@@ -70,68 +47,27 @@ namespace Player
             var shotAngle = Vector2.SignedAngle(Vector2.right * direction, shotVector)*Mathf.Sign(direction);
             var shotAngle01 = Mathf.Clamp01(Mathf.InverseLerp(maxAim, minAim, shotAngle));
             bodyAnimator.SetFloat("Aim Angle", shotAngle01);
-            if (Input.GetMouseButtonDown(0) || (automaticFire && Input.GetMouseButton(0)))
+            var aimFrame = shotAngle switch
             {
-                FireIfPossible(aimPosition, shotAngle);
-            }
-        }
-
-        private void FireIfPossible(Vector2 aimPoint, float angle)
-        {
-            if (activeCooldown > 0) return;
-            if (angle < minAim || angle > maxAim) return;
-            
-            Vector2 muzzlePos = muzzle.transform.position;
-
-            var shotVector = aimPoint - muzzlePos;
-            var shotDirection = shotVector.normalized;
-        
-            // Adjust shot point to max range
-            var dist = shotVector.magnitude;
-            if (dist > maxRange)
+                > 33.75f => 1,
+                > 11.25f => 2,
+                > -11.25f => 3,
+                > -33.75f => 4,
+                _ => 5
+            };
+            bodyAnimator.SetInteger("Aim Frame", aimFrame);
+            if (shotAngle < minAim || shotAngle > maxAim) return;
+            if (Input.GetMouseButtonDown(0) || (weapon.automaticFire && Input.GetMouseButton(0)))
             {
-                shotVector = shotDirection * maxRange;
+                weapon.FireIfPossible(aimPosition, shotAngle);
             }
-
-            activeCooldown = cooldownBetweenShots;
-            muzzleFlash.transform.rotation = Quaternion.Euler(0, 0, angle);
-            muzzleFlash.SetTrigger(Shoot);
-            
-            audioPlayer.PlayOneShot(shootSound);
-
-            Vector3 impactPos = muzzlePos + shotVector;
-            
-            var hit = Physics2D.Raycast(muzzlePos, shotDirection, maxRange);
-            if (hit.collider != null)
-            {
-                impactPos = hit.point;
-                if (CheckWhatWasHit(hit.collider.gameObject, hit.point))
-                {
-                    return;
-                }
-            }
-
-            impactPos.z = transform.position.z;
-            
-            // Wasteful, use object pool
-            var projectileImpact = Instantiate(projectileImpactPrefab);
-            projectileImpact.transform.position = impactPos;
-            Destroy(projectileImpact, projectileImpactDuration);
-        }
-
-        private bool CheckWhatWasHit(GameObject hitObject, Vector2 hitPoint)
-        {
-            var health = hitObject.GetComponentInParent<Health>();
-            if (health == null) return false;
-            health.Damage(damage, hitObject, hitPoint);
-            return true;
         }
 
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.yellow;
             var p0 = aimRotationPoint.transform.position;
-            var arcLen = maxRange;
+            var arcLen = weapon.maxRange;
             var p1 = p0 + arcLen * new Vector3(Mathf.Cos(minAim * Mathf.Deg2Rad), Mathf.Sin(minAim * Mathf.Deg2Rad), 0);
             var p2 = p0 + arcLen * new Vector3(Mathf.Cos(maxAim * Mathf.Deg2Rad), Mathf.Sin(maxAim * Mathf.Deg2Rad), 0);
             Gizmos.DrawLine(p0, p1);
