3f241d335886791bc8c492d9536e92b809f3cfde
Johan Östling <johan.f.ostling@gmail.com>
2022-06-16 17:11:08 +0200

    Make gradual text reveal optional

--- Assets/Scripts/UI/RadioMessagesView.cs
+++ Assets/Scripts/UI/RadioMessagesView.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using Channels;
 using Narrative;
 using UnityEngine;
 using UnityEngine.UIElements;
-using System.Text;
 
 namespace UI
 {
@@ -12,15 +12,15 @@ namespace UI
     {
         public NarrationChannel radioMessages;
         public AudioSource audioSource;
+        public bool gradualReveal;
 
         private RadioMessagesController radioMessagesController;
         private Stack<Narration> narrations = new();
-        private Coroutine revealCoroutine;
 
         private void OnEnable()
         {
             var ui = GetComponent<UIDocument>();
-            radioMessagesController = new (ui.rootVisualElement);
+            radioMessagesController = new(ui.rootVisualElement);
             radioMessagesController.Clear();
             radioMessages.AddSubscriber(this);
         }
@@ -47,32 +47,31 @@ namespace UI
                 var narration = narrations.Pop();
                 while (narration != null)
                 {
-                    // radioMessagesController.Show(narration.text);
-                    StartCoroutine(
-                        radioMessagesController.RevealText(
-                            narration.text.ToCharArray(),
-                            narration.audioClip != null ? narration.audioClip.length : narration.defaultDuration
-                        ));
+                    var delay = narration.defaultDuration;
 
                     if (narration.audioClip != null)
                     {
+                        delay = Mathf.Max(delay, narration.audioClip.length);
                         audioSource.PlayOneShot(narration.audioClip);
                     }
-                    yield return new WaitForSeconds(narration.defaultDuration);
-                    while (audioSource.isPlaying)
-                    {
-                        yield return new WaitForFixedUpdate();
-                    }
 
-                    if (narration.next != null)
+                    if (gradualReveal)
                     {
-                        yield return new WaitForSeconds(narration.delayUntilNext);
-                        narration = narration.next;
+                        yield return radioMessagesController.RevealText(
+                            narration.text.ToCharArray(),
+                            delay
+                        );
                     }
                     else
                     {
-                        break;
+                        radioMessagesController.Show(narration.text);
+                        yield return new WaitForSeconds(delay);
                     }
+
+                    radioMessagesController.Clear();
+                    if (narration.next == null) break;
+                    yield return new WaitForSeconds(narration.delayUntilNext);
+                    narration = narration.next;
                 }
             }
         }
@@ -81,6 +80,7 @@ namespace UI
     internal class RadioMessagesController
     {
         private readonly Label label;
+
         public RadioMessagesController(VisualElement root)
         {
             label = root.Q<Label>("RadioMessages");
@@ -99,7 +99,7 @@ namespace UI
         public IEnumerator RevealText(char[] textToReveal, float audioClipLength)
         {
             int count = 0;
-            float wait = audioClipLength / (float)textToReveal.Length * 0.85f;
+            float wait = audioClipLength / (float) textToReveal.Length * 0.85f;
             StringBuilder displayText = new StringBuilder(textToReveal.Length);
 
             while (count < textToReveal.Length)
@@ -110,10 +110,6 @@ namespace UI
 
                 yield return new WaitForSeconds(wait);
             }
-
-            yield return new WaitForSeconds(1);
-
-            Clear();
         }
     }
 }
