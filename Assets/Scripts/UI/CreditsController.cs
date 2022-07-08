a778bc1816677fcc687c161a4da5841edb5d504e
Johan Östling <johan.f.ostling@gmail.com>
2022-07-08 14:37:13 +0200

    Add credits

---
+++ Assets/Scripts/UI/CreditsController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class CreditsController
+    {
+        private readonly Button closeButton;
+        private readonly Label creditDisplay;
+        private readonly VisualElement buttonHolder;
+
+        public CreditsController(VisualElement root, VisualTreeAsset creditButtonTemplate, string[] creditNames,
+            string[] creditContents)
+        {
+            closeButton = root.Q<Button>("CloseButton");
+            buttonHolder = root.Q<VisualElement>("NameButtons");
+            creditDisplay = root.Q<Label>("Credits");
+            
+            for (var i = 0; i < creditNames.Length; i++)
+            {
+                var buttonElement = creditButtonTemplate.Instantiate();
+                var button = buttonElement.Q<Button>();
+                button.text = creditNames[i];
+                var creditDisplayText = creditContents[i];
+                button.clicked += () => { creditDisplay.text = creditDisplayText; };
+                buttonHolder.Add(buttonElement);
+            }
+        }
+
+        public void SetCloseAction(UnityEvent closeEvent)
+        {
+            closeButton.clicked += closeEvent.Invoke;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/CreditsView.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Channels;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class CreditsView : MonoBehaviour
+    {
+        public TextAsset[] credits;
+        public BoolChannel gameRunning;
+
+        public VisualTreeAsset creditButtonTemplate;
+
+        public UnityEvent onClose;
+
+        private void OnEnable()
+        {
+            gameRunning.Push(false);
+            var ui = GetComponent<UIDocument>();
+            var creditNames = credits.Select(c => c.text.Split("\n")[0]).ToArray();
+            var creditContents = credits.Select(c => c.text).ToArray();
+            var controller = new CreditsController(ui.rootVisualElement, creditButtonTemplate, creditNames, creditContents);
+            controller.SetCloseAction(onClose);
+        }
+
+        private void OnDisable()
+        {
+            gameRunning.Push(true);
+        }
+    }
+}
