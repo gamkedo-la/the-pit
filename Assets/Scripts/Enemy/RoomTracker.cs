8691ffaa64f19e2c26ad2a54c886072b25f9dc85
Johan Östling <johan.f.ostling@gmail.com>
2022-07-09 19:40:00 +0200

    Reparent moving monsters to the room they are in to make sure they move with the room if level wrapping occurs

---
+++ Assets/Scripts/Enemy/RoomTracker.cs
@@ -0,0 +1,28 @@
+﻿using Level;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class RoomTracker : MonoBehaviour
+    {
+        public PitLevel level;
+
+        private void Start()
+        {
+            if (level == null)
+            {
+                level = FindObjectOfType<PitLevel>();
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (!level.FindRoom(transform.position, out var room)) return;
+
+            if (room.transform != transform.parent)
+            {
+                transform.parent = room.transform;
+            }
+        }
+    }
+}
