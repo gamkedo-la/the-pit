45480f7c3da870bc830cb52bc7a6240bf5e48f97
Johan Östling <johan.f.ostling@gmail.com>
2022-07-09 19:17:40 +0200

    Show mission complete UI on mission completed

---
+++ Assets/Scripts/Util/TriggerDelayed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Util
+{
+    public class TriggerDelayed : MonoBehaviour
+    {
+        public UnityEvent onTrigger;
+        
+        public void SetActive(float delay)
+        {
+            StartCoroutine(DelayTrigger(delay));
+        }
+
+        private IEnumerator DelayTrigger(float delay)
+        {
+            yield return new WaitForSecondsRealtime(delay);
+            onTrigger.Invoke();
+        }
+    }
+}
